57300c6abb7c69d7ff2739c17b1a290692f0d197
William Huang <wilhu@upenn.edu>
2025-02-09 10:06:24 -0500

    form handler method for login component

--- src/LoginComponent/LoginComponent.js
+++ src/LoginComponent/LoginComponent.js
@@ -2,20 +2,28 @@ import React from 'react'
 
 
 function LoginComponent() {
-	
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		const password = document.getElementById('floatingPassword').value
+		if (password === 'angel') {
+			alert('correct password! welcome Angel!')
+		} else {
+			alert('Wrong password! Try again.')
+		}
+	}
   return (
 	<div class="modal modal-sheet påosition-static d-block bg-body-secondary p-4 py-md-5" tabindex="-1" role="dialog" id="modalSignin">
 		<div class="modal-dialog" role="document">
 		  <div class="modal-content rounded-4 shadow">
 			<div class="modal-header p-5 pb-4 border-bottom-0">
-			  <h1 class="fw-bold mb-0 fs-2">This page is for Angel. Others are not allowed.</h1>
+			  <h1 class="fw-bold mb-0 fs-2">This page is for Angel only! Others are not allowed.</h1>
 			</div>
 
 			<div class="modal-body p-5 pt-0">
-			  <form class="">
+			  <form class="" onSubmit={handleSubmit}>
 				<div class="form-floating mb-3">
 				  <input type="password" class="form-control rounded-3" id="floatingPassword" placeholder="Password"/>
-					<label for="floatingPassword">Password</label>
+					<label for="floatingPassword">Secret Password</label>
 				</div>
 				<button class="w-100 mb-2 btn btn-lg rounded-3 btn-primary" type="submit">Log in</button>
 			  </form>
