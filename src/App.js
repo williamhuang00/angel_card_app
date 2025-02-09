03297e1303c625992901b040bf8b4a809d8082ca
William Huang <wilhu@upenn.edu>
2025-02-09 10:06:41 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginComponent from './LoginComponent/LoginComponent';
-
+import CardComponent from './CardComponent/CardComponent';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <LoginComponent/>
+      <Router>
+      <Routes>
+        <Route path="/" element={<LoginComponent/>} />
+        <Route path="/card" element={<CardComponent/>} />
+      </Routes>
+    </Router>
     </div>
   );
 }
