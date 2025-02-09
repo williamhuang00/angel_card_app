c344a2c96f12ffdde0cc98c35eb3b01f0ea3dee4
William Huang <wilhu@upenn.edu>
2025-02-09 10:11:05 -0500

    form navigation to card route after login

--- src/LoginComponent/LoginComponent.js
+++ src/LoginComponent/LoginComponent.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 function LoginComponent() {
+	const navigate = useNavigate()
+
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		const password = document.getElementById('floatingPassword').value
 		if (password === 'angel') {
 			alert('correct password! welcome Angel!')
+			navigate('/card')
 		} else {
 			alert('Wrong password! Try again.')
 		}
