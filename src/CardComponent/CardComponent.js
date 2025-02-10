6ffd37f8ce08663adced9a8702176885384e2d7f
William Huang <wilhu@upenn.edu>
2025-02-09 21:38:11 -0500

    card component final changes. footer pagination

--- src/CardComponent/CardComponent.js
+++ src/CardComponent/CardComponent.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
-import image from "../Images/simpleplan.jpg"
+import gingerbread_img from "../Images/gingerbreadman.png"
+import mewow_cafe from "../Images/mewow_cafe.jpg"
 import './CardComponent.css'
 
 function CardComponent() {
@@ -8,21 +9,27 @@ function CardComponent() {
     const data = [
         {
             "id": 0,
-            "title": "Simple Plan",
-            "description": "Simple Description",
-            "image": image
+            "title": "Hi Angel",
+            "description": "I know we haven't known each other for very long, but even still I wanted to do something special for you for valentine's day. I hope you like it!",
+            "image": null
         },
         {
             "id": 1,
-            "title": "Another Plan",
-            "description": "Another Description",
-            "image": image
+            "title": "",
+            "description": "You're so silly and cheerful when you talk about the things you like.",
+            "image": gingerbread_img
         },
         {
             "id": 2,
-            "title": "Third Plan",
-            "description": "Third Description",
-            "image": image
+            "title": "",
+            "description": "Whether it's about cats, psychology, or philly sports (I'll learn about it) I'd love to keep talking and spend more time together.",
+            "image": mewow_cafe
+        },
+        {
+            "id": 3,
+            "title": "That's all for now",
+            "description": "I hope you enjoyed this little surprise. Happy Valentines day!",
+            "image": null
         }
     ]
 
@@ -32,13 +39,19 @@ function CardComponent() {
         <div class="container">
             <div class="bg-body-tertiary p-5 rounded">
                 <h1>{currentData.title}</h1>
-                <p class="lead">{currentData.description}</p>
-                <img src={currentData.image} alt="" width="100%" height="100%"/>
+                {currentData.image && (
+                    <img src={currentData.image} alt="" width="100%" height="100%"/>
+                )}
+                
+                <p class="lead" style={{marginTop: '5%'}}>{currentData.description}</p>
+                {
+                    currentId === data.length - 1 && 
+                    <p class="lead" style={{marginTop: '5%'}}>{"- William"}</p>
+                }
+
             </div>
         </div>
-
-
-        <nav aria-label="Page navigation example">
+        <nav aria-label="Page navigation example" class="fixed-bottom">
             <ul class="pagination justify-content-center">
                 <li className={`page-item ${currentId === 0 ? 'disabled' : ''}`}>
                         <a className="page-link" onClick={() => setCurrentId(currentId - 1)}>Previous</a>
